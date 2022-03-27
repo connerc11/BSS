79989004d6a6b2b68e9204bded42bedab4569a5e
Conner Cochrane <cccochrane1@knights.ucf.edu>
2022-03-27 14:27:45 -0400

    working on cart and indexDB

---
+++ BSS/client/src/components/CartItem/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+
+const CartItem = ({ item }) => {
+  const [, dispatch] = useStoreContext();
+
+const removeFromCart = item => {
+  dispatch({
+    type: REMOVE_FROM_CART,
+    _id: item._id
+  });
+};
+  return (
+    <div className="flex-row">
+      <div>
+        <img
+          src={`/images/${item.image}`}
+          alt=""
+        />
+      </div>
+      <div>
+        <div>{item.name}, ${item.price}</div>
+        <div>
+        <span
+  role="img"
+  aria-label="trash"
+  onClick={() => removeFromCart(item)}
+>
+  🗑️
+</span>
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+          />
+          <span
+            role="img"
+            aria-label="trash"
+          >
+            🗑️
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
--- BSS/client/src/components/ProductList/index.js
+++ BSS/client/src/components/ProductList/index.js
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
-
+import { idbPromise } from "../../utils/helpers";
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 
-function ProductList({ currentCategory }) {
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+function ProductList() {
+  const [state, dispatch] = useStoreContext();
 
-  const products = data?.products || [];
+const { currentCategory } = state;
 
-  function filterProducts() {
-    if (!currentCategory) {
-      return products;
-    }
+const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-    return products.filter(
-      (product) => product.category._id === currentCategory
-    );
+useEffect(() => {
+  if(data) {
+    dispatch({
+      type: UPDATE_PRODUCTS,
+      products: data.products
+    });
+
+    data.products.forEach((product) => {
+      idbPromise('products', 'put', product);
+    });
+    // add else if to check if `loading` is undefined in `useQuery()` Hook
+  } else if (!loading) {
+    // since we're offline, get all of the data from the `products` store
+    idbPromise('products', 'get').then((products) => {
+      // use retrieved data to set global state for offline browsing
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: products
+      });
+    });
+  }
+}, [data, loading, dispatch]);
+
+
+function filterProducts() {
+  if (!currentCategory) {
+    return state.products;
+  }
+
+  return state.products.filter(product => product.category._id === currentCategory);
+
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
